65f63e2438665fdfe97ef1bebed9e288437e77f7
Methminpulsara <methminpulsara10@gmail.com>
2025-07-19 11:49:02 +0530

    fist commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# print("hello im "'"Methmin"'" ")
+
+# print('hello im "Methmin"')
+
+# print("hello im \"Methmin\" ")
+
+# print("hello \n hello ")
+
+# print("hello world \n" * 5)
+
+
+my_name = "methmin"
+age = 18
+is_student = False
+height = 1.56
+#
+# print(my_name, age, is_student, height)
+# print(f"{my_name} {age} {is_student} {height} ")
+# print(my_name  + " "  +str(age)  )
+
