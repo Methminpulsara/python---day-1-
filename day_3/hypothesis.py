902225e76b24dfb0e651e4b4f8521af449d4b2b4
Methminpulsara <methminpulsara10@gmail.com>
2025-08-02 09:14:08 +0530

    update

---
+++ day_3/hypothesis.py
@@ -0,0 +1,12 @@
+c0 = int(input("Enter a positive number: "))
+
+if c0 > 1:
+    while c0 != 1:
+        print(c0)
+        if c0 % 2 == 0:
+            c0 = c0 // 2
+        else:
+            c0 = 3 * c0 + 1
+    print(c0)
+else:
+    print(" invalid input ")
