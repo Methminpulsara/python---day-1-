f3310275a061fb3df944e6ecefed74e02a317869
Methminpulsara <methminpulsara10@gmail.com>
2025-08-09 12:02:24 +0530

    update

--- day_4/list_comprehension.py
+++ day_4/list_comprehension.py
@@ -1,8 +1,13 @@
-numbers = [i * 8 for i in  range(1,10)]
-print(numbers)
+# numbers = [i * 8 for i in  range(1,10)]
+# print(numbers)
+#
+#
+# word_arr = [ word for i in range(110)]
+# print(word_arr)
+#
+# numbers_divisible_by_3 = [ ]
 
-
-word_arr = [ word for i in range(110)]
-print(word_arr)
-
-numbers_divisible_by_3 = [ ]
+list = [73045,3453,353,345,345]
+array = [1 , 3 , "hello" , True , False , 0.9333 ,list]
+for i in array:
+    print(i)
