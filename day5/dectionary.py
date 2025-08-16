91721044762ebcf386414e5bd7335aef2d835324
Methminpulsara <methminpulsara10@gmail.com>
2025-08-16 11:37:20 +0530

    update

---
+++ day5/dectionary.py
@@ -0,0 +1,23 @@
+student = {"name" :  "methmin" ,
+            "age" : 18 ,
+           "address" : "horana",
+           "school" : "ds",
+           "contact" :  ["07213114", "071231231" , "0712323111"]
+           }
+#
+# print(student.get("name"))
+#
+# student["maried_status"] = "yes"
+#
+# print(student)
+#
+# student["name"] = "test"
+#
+# print("after change name" , student["name"])
+
+
+print(student["contact"][2])
+
+student["contact"].append("0719189399")
+
+print("after adding new phone number " , student["contact"])
