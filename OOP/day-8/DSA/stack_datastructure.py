394893680b820ca63391afd826418e7ab2264838
Methminpulsara <methminpulsara10@gmail.com>
2025-09-06 09:24:27 +0530

    update

---
+++ OOP/day-8/DSA/stack_datastructure.py
@@ -0,0 +1,25 @@
+stack = []
+
+
+def push(value):
+    return stack.append(value)
+
+def pop():
+
+    if is_empty() :
+        return "Cant remove Elements !"
+    value = stack[-1]
+    del stack[-1]
+    return value
+
+def is_empty():
+    return len(stack) == 0
+
+
+
+push(1)
+push(2)
+
+print("Pop = ", pop())
+
+print(stack)
