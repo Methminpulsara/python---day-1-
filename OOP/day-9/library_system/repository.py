6fb14e042fb366b845415a2c80cdc244edd8f9e1
Methminpulsara <methminpulsara10@gmail.com>
2025-09-20 14:37:31 +0530

    update

--- OOP/day-9/library_system/repository.py
+++ OOP/day-9/library_system/repository.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List
-from models import Book
+
+from models import *
 
 
 class BookRepository(ABC):
@@ -19,4 +20,52 @@ class BookRepository(ABC):
 
     @abstractmethod
     def get_all_books(self) -> List[Book]:
+        pass
+
+
+class MemberRepository(ABC):
+
+    @abstractmethod
+    def add_member(self, member: Member) -> None:
+        pass
+
+    @abstractmethod
+    def get_member_by_id(self, member_id) -> Member:
+        pass
+
+    @abstractmethod
+    def get_member_list(self) -> List[Member]:
+        pass
+
+
+class LoanRepository(ABC):
+
+    @abstractmethod
+    def add_loan(self, loan: Loan) -> None:
+        pass
+
+    @abstractmethod
+    def get_by_id(self, loan_id) -> Loan:
+        pass
+
+    @abstractmethod
+    def update_loan(self, book: Loan) -> None:
+        pass
+
+    @abstractmethod
+    def get_all_loans(self) -> List[Loan]:
+        pass
+
+
+class InMemoryBookRepository(BookRepository):
+    def get_by_id(self, book_id) -> Book:
+        pass
+
+    def update_book(self, book: Book) -> None:
+        pass
+
+    def get_all_books(self) -> List[Book]:
+        pass
+
+    def add_book(self, book: Book) -> None:
         pass
