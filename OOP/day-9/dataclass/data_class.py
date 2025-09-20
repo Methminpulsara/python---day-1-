099be9b4ac6c29d5a07ff505e51e839c6c924a60
Methminpulsara <methminpulsara10@gmail.com>
2025-09-20 11:56:22 +0530

    update

---
+++ OOP/day-9/dataclass/data_class.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Student:
+    name : str
+    age : int
+    contact : str
+    course : str
+    grades : List[int]
+
+    def add_grade(self, grade:int) -> None:
+        self.grades.append(grade)
+
+    def get_avg(self) -> float:
+        if len(self.grades) == 0 :
+            return sum(self.grades) / len(self.grades)
+
+
+student = Student("Methmin"  ,18 , "0719189399" , "It" , 10)
+
+print(student)
