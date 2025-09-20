003bba655eb46e9071fe58194a525f3f58468f7d
Methminpulsara <methminpulsara10@gmail.com>
2025-09-20 14:58:14 +0530

    update

--- OOP/day-9/library_system/repository.py
+++ OOP/day-9/library_system/repository.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Dict
 
 from models import *
 
@@ -58,14 +58,50 @@ class LoanRepository(ABC):
 
 
 class InMemoryBookRepository(BookRepository):
+
+    def __init__(self):
+        self.__books: Dict[str, Book] = {}
+
     def get_by_id(self, book_id) -> Book:
-        pass
+        return self.__books.get(book_id)
 
     def update_book(self, book: Book) -> None:
-        pass
+        self.__books[book.book_id] = book
 
     def get_all_books(self) -> List[Book]:
-        pass
+        # books.values  -> objects thiyan innw
+        return list(self.__books.values())
 
     def add_book(self, book: Book) -> None:
-        pass
+        self.__books[book.book_id] = book
+
+
+class InMemoryMemberRepository(MemberRepository):
+    def __init__(self):
+        self.members: Dict[str, Member] = {}
+
+    def add_member(self, member: Member) -> None:
+        self.members[member.member_id] = member
+
+    def get_member_by_id(self, member_id) -> Member:
+        return self.members.get(member_id)
+
+    def get_member_list(self) -> List[Member]:
+        return list(self.members.values())
+
+
+class InMemoryLoanRepository(LoanRepository):
+    def __init__(self):
+        self.loans: Dict[str, Loan] = {}
+
+    def add_loan(self, loan: Loan) -> None:
+        self.loans[loan.loan_id] = loan
+
+    def get_by_id(self, loan_id) -> Loan:
+        return self.loans.get(loan_id)
+
+    def update_loan(self, loan: Loan) -> None:
+        self.loans[loan.loan_id] = loan
+
+    def get_all_loans(self) -> List[Loan]:
+        return list(self.loans.values())
