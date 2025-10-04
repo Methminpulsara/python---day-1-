f98cc8343b4a03e30efd843f07eab1f06bddd8ac
Methminpulsara <methminpulsara10@gmail.com>
2025-10-04 09:06:06 +0530

    inizial commit

--- OOP/day_11/lamda_funtion/lamda.py
+++ OOP/day_11/lamda_funtion/lamda.py
@@ -11,3 +11,13 @@ math_operators = {
 
 for key , val in math_operators.items():
     print(f"{key} -  {val(100 , 200)}")
+
+
+nums = [1 , 2 , 3 , 4 , 4 , 5, 6 , 6]
+
+
+nams_doubled_map = list(map(lambda x : x * 2 , nums))
+print("Doubled numbers -", nams_doubled_map)
+
+filtered_nums = list(map(lambda x : x % 2 == 0 , nums))
+print(filtered_nums)
